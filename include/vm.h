b27573e4221e8f67b46ae720610de89ecef56eea
lucas-mansson <lu5132ma-s@student.lu.se>
2026-06-24 19:55:07 +0200

    add instruction stack

--- include/vm.h
+++ include/vm.h
@@ -2,11 +2,16 @@
 #define vm_h
 
 #include "chunk.h"
+#include "value.h"
 #include <stdint.h>
+
+#define STACK_MAX 256
 
 typedef struct {
     chunk* chunk;
-    uint8_t* ip; // instruction pointer
+    uint8_t* ip; // instruction pointer, points to next code to be executed
+    value stack[STACK_MAX]; // instruction stack
+    value* stack_top;       // one past the top element in the stack
 } virtual_machine;
 
 typedef enum {
@@ -15,9 +20,12 @@ typedef enum {
     INTERPRET_RUNTIME_ERROR
 } interpret_result;
 
-void init_vm();
-void free_vm();
+void init_vm(void);
+void free_vm(void);
 
 interpret_result interpret(chunk* chunk);
+
+void push(value val);
+value pop(void);
 
 #endif
--- src/vm.c
+++ src/vm.c
@@ -1,30 +1,60 @@
 #include "vm.h"
 #include "chunk.h"
 #include "common.h"
+#include "debug.h"
 #include "value.h"
 #include <stdio.h>
 
 virtual_machine vm;
 
-void init_vm() {}
+void reset_stack(void) { vm.stack_top = vm.stack; }
 
-void free_vm() {}
+void init_vm(void) { reset_stack(); }
 
-static interpret_result run()
+void free_vm(void) {}
+
+void push(value val)
+{
+    *vm.stack_top++ = val;
+    vm.stack_top++;
+}
+
+value pop(void)
+{
+    vm.stack_top--;
+    return *vm.stack_top;
+}
+
+static interpret_result run(void)
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
     while (true) {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("    ");
+        // print each value in stack each iteration
+        for (value* slot = vm.stack; slot < vm.stack_top; slot++) {
+            printf("[");
+            print_value(*slot);
+            printf("]");
+        }
+        printf("\n");
+        disassemble_instruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
         uint8_t instruction = READ_BYTE();
         switch (instruction) {
+
         case OP_CONSTANT: {
             value constant = READ_CONSTANT();
-            print_value(constant);
-            printf("\n");
+            push(constant);
             break;
         }
+
         case OP_RETURN: {
+            print_value(pop());
+            printf("\n");
             return INTERPRET_OK;
         }
         }
