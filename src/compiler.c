6f57e5bb045119e425a1b86de130e66bbc72bf94
lucas-mansson <lu5132ma-s@student.lu.se>
2026-07-07 21:04:44 +0200

    add tokens

--- src/compiler.c
+++ src/compiler.c
@@ -1,2 +1,25 @@
+#include "scanner.h"
+#include <stdbool.h>
+#include <stdio.h>
 
-void compile(const char* source) { init_scanner(source); }
+void compile(const char* source)
+{
+    init_scanner(source);
+    int line = -1;
+
+    while (true) {
+        token t = scan_token();
+
+        if (t.line != line) {
+            printf("%4d", t.line);
+            line = t.line;
+        } else {
+            printf("    | ");
+        }
+        printf("%2d '%.*s'\n", t.type, t.length, t.start);
+
+        if (t.type == TOKEN_EOF) {
+            break;
+        }
+    }
+}
--- src/scanner.c
+++ src/scanner.c
@@ -1,4 +1,6 @@
 #include "scanner.h"
+#include <stdbool.h>
+#include <string.h>
 
 typedef struct {
     const char* start; // start of current lexeme
@@ -13,4 +15,38 @@ void init_scanner(const char* source)
     scan.start = source;
     scan.curr = source;
     scan.line = 1;
+}
+
+static bool is_at_end(void) { return *scan.curr == '\0'; }
+
+static token make_token(token_type type)
+{
+    token t;
+    t.type = type;
+    t.start = scan.start;
+    t.length = (scan.curr - scan.start);
+    t.line = scan.line;
+
+    return t;
+}
+
+static token error_token(const char* msg)
+{
+    token t;
+    t.type = TOKEN_ERROR;
+    t.start = msg;
+    t.length = strlen(msg);
+    t.line = scan.line;
+
+    return t;
+}
+
+token scan_token(void)
+{
+    scan.start = scan.curr;
+    if (is_at_end()) {
+        return make_token(TOKEN_EOF);
+    }
+
+    return error_token("Unexpected character");
 }
